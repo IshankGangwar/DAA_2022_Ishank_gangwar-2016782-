475da387d3dbb14059421ff5ac6f557148a2123c
IshankGangwar <101080370+IshankGangwar@users.noreply.github.com>
2022-04-11 10:08:02 +0530

    Update daa1.cpp

--- daa1.cpp
+++ daa1.cpp
@@ -12,12 +12,9 @@ cin>>key;
 for(int i=0;i<n;i++){
     if(a[i]==key){
             nooftimes++;
-
-
     }
     if(nooftimes==0){
         cout<<"key is not present"<<endl;
-
     }
     else{
         cout<<"key is present"<<endl;
